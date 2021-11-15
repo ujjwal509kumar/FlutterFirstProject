babfc0c46ff63611350a73ec02b030a4cab59d41
Ujjwal Kumar <ujjwal509kumar@gmail.com>
2021-11-15 15:49:55 +0530

    Themes Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:first_app/home_page.dart';
 import 'package:first_app/login_page.dart';
 import 'package:first_app/routes.dart';
+import 'package:first_app/themes.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 void main() {
@@ -12,13 +13,8 @@ class firstApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily,
-        ),
-       darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        ),
+      theme: MyTheme.lightTheme(context),
+       darkTheme: MyTheme.darkTheme(context),
         debugShowCheckedModeBanner: false,
         initialRoute: MyRoutes.homeRoute,
         routes: {
