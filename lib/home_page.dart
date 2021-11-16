2d225a73a8f0f8217e5cbb8319f46263b0ece482
Ujjwal Kumar <ujjwal509kumar@gmail.com>
2021-11-16 21:30:02 +0530

    day-12 completed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -35,14 +35,39 @@ class _HomepageState extends State<Homepage> {
       ),
         body: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: (ItemModel.items!=null && ItemModel.items.isNotEmpty)? ListView.builder(
-            itemCount: ItemModel.items.length,
-            itemBuilder: (BuildContext context, int index) { 
-              return ItemWidget(
-                item: ItemModel.items[index],
-              );
-             },
-          ):Center(child: CircularProgressIndicator(),),
+          child: (ItemModel.items!=null && ItemModel.items.isNotEmpty)
+          ? GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+             mainAxisSpacing: 16,
+             crossAxisSpacing: 16,
+          ), 
+          itemBuilder: (context,index){
+            final item = ItemModel.items[index];
+            return Card(
+              clipBehavior: Clip.antiAlias,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              child: GridTile(
+                header: Container(child: Text(item.name, style: TextStyle(color: Colors.white),
+                ),
+                padding: const EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple,
+                ),
+                ),
+                child: Image.network(item.image,),
+                footer: Container(child: Text(item.price.toString(), style: TextStyle(color: Colors.black),
+                ),
+                padding: const EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple,
+                ),
+                ),
+                )
+                );
+          },
+          itemCount: ItemModel.items.length,)
+          : Center(child: CircularProgressIndicator(),
+          ),
         ),
         drawer: MyDrawer(),
       );
